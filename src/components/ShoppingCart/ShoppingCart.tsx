1b80b24501b01dafb8f75b59a50dc6b0f563bcd1
Felipe França <felipe.franca.108@ufrn.edu.br>
2023-04-21 21:17:30 -0300

    add a fill function to fill the shopping cart, only showing the items conditionally if the item is in the cart

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,32 +1,56 @@
-import CartItem from "../CartItem";
 import mockItems from "../../assets/menu.json";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ShoppingCartContext } from "../../context/ShoppingCartContext";
 import ShoppingSummary from "../ShoppingSummary/ShoppingSummary";
 import PreviousOrders from "../PreviousOrders/PreviousOrders";
+import CartItem from "../CartItem/CartItem";
 
 const ShoppingCart = () => {
-  const { cartTotalItems } = useContext(ShoppingCartContext);
+  const { cartTotalItems, increaseCartQuantity, cartItems } =
+    useContext(ShoppingCartContext);
+
+  const handleInitializeCart = () => {
+    if (cartItems.length === 0) {
+      return mockItems.map((item) => {
+        increaseCartQuantity(item.id);
+      });
+    }
+
+    return alert("Carrinho já preenchido");
+  };
 
   return (
     <div className="w-full">
       <header className="p-4 pt-8">
-        <h1 className="text-2xl">Carrinho de compras</h1>
+        <div className="flex gap-4">
+          {" "}
+          <h1 className="text-2xl">Carrinho de compras</h1>
+          <button
+            className="bg-[#4DE1C1] hover:bg-[#62eed0] active:bg-[#2bb194] p-2 text-sm rounded-md h-8 outline-none"
+            type="submit"
+            onClick={handleInitializeCart}
+          >
+            Preencher Carrinho
+          </button>
+        </div>
         <span>Você tem {cartTotalItems} itens.</span>
       </header>
       <div className="flex sm:flex-col lg:flex-row">
         <div className="flex flex-col gap-2 mx-4">
-          {mockItems.map((item, index) => (
-            <div key={item.name + index}>
-              <CartItem
-                id={item.id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                imageUrl={item.imageUrl}
-              />
-            </div>
-          ))}
+          {mockItems.map(
+            (item, index) =>
+              cartItems.find((cartItem) => cartItem.id === item.id) && (
+                <div key={item.name + index}>
+                  <CartItem
+                    id={item.id}
+                    name={item.name}
+                    description={item.description}
+                    price={item.price}
+                    imageUrl={item.imageUrl}
+                  />
+                </div>
+              )
+          )}
         </div>
         <aside className="flex lg:flex-col sm:p-5 lg:p-0 sm:flex-row-reverse gap-4">
           <ShoppingSummary />
